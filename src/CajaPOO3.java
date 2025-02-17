54f6230fe6ab64b8d81f4bec943ea5745f8aacd6
LkzFer72 <acxel271212@gmail.com>
2025-02-17 15:21:17 -0700

    Act3. Se agregó el método setDimensions, POO2, y el método constructor en POO3.

--- src/CajaPOO3.java
+++ src/CajaPOO3.java
@@ -18,5 +18,13 @@ public class CajaPOO3 {
         this.alto= alto;
 
     }
+    public CajaPOO3(){
+        this.largo = 0;
+        this.ancho = 0;
+        this.alto= 0;
+
+    }
+
+
 
 }
--- src/PruebaCaja.java
+++ src/PruebaCaja.java
@@ -9,29 +9,33 @@ public class PruebaCaja {
         System.out.println("El volumen de la caja es: " + miCaja.calcularVolumen());
 
         //Caja 2/
-        CajaPOO2 miCajax = new CajaPOO2();
-        miCajax.largo = 10.0;
-        miCajax.ancho = 4.0;
-        miCajax.alto = 5.0;
+        CajaPOO2 miCaja2 = new CajaPOO2();
+        miCaja2.largo = 10.0;
+        miCaja2.ancho = 4.0;
+        miCaja2.alto = 5.0;
 
-        System.out.println("El volumen de la caja es: " + miCajax.calcularVolumen());
+        System.out.println("El volumen de la caja es: " + miCaja2.calcularVolumen());
 
         System.out.println("El volumen de la caja 1 es "+miCaja.calcularVolumen());
-        System.out.println("El volumen de la caja 2 es "+miCajax.calcularVolumen());
+        System.out.println("El volumen de la caja 2 es "+miCaja2.calcularVolumen());
 
-        CajaPOO2 miCajay = new CajaPOO2();
-        miCajay.setDimensiones(5, 3, 2);
-        System.out.println("El volumen de mi caja 3 es "+miCajay.calcularVolumen());
+        CajaPOO2 miCaja3 = new CajaPOO2();
+        miCaja3.setDimensiones(5, 3, 2);
+        System.out.println("El volumen de mi caja 3 es "+miCaja3.calcularVolumen());
 
-        CajaPOO2 miCajaz = new CajaPOO2();
-        miCajaz.setDimensiones(10, 4, 5);
-        System.out.println("El volumen de mi caja 4 es "+miCajaz.calcularVolumen());
+        CajaPOO2 miCaja4 = new CajaPOO2();
+        miCaja4.setDimensiones(10, 4, 5);
+        System.out.println("El volumen de mi caja 4 es "+miCaja4.calcularVolumen());
 
         //Instanciar CajaPOO3/
-        CajaPOO3 miCajaw = new CajaPOO3(5, 3, 2);
-        System.out.println("El volumen de mi caja 5 es "+miCajaw.calcularVolumen());
+        CajaPOO3 miCaja5 = new CajaPOO3(5, 3, 2);
+        System.out.println("El volumen de mi caja 5 es "+miCaja5.calcularVolumen());
 
-        CajaPOO3 miCajav = new CajaPOO3(10, 4, 9);
-        System.out.println("El volumen de la caja 6 es "+miCajav.calcularVolumen());
+        CajaPOO3 miCaja6 = new CajaPOO3(10, 4, 9);
+        System.out.println("El volumen de la caja 6 es "+miCaja6.calcularVolumen());
+
+        //Ejemplo de variables public y private/
+        CajaPOO3 miCaja7 = new CajaPOO3();
+
     }
 }
